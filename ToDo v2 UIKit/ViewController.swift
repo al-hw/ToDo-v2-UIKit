ad473751acc8c8379c7a13524a8e8b641c7438ef
Alex Hwan <for.hwan@gmail.com>
2022-11-18 20:05:26 +0600

    Initial Commit

---
+++ ToDo v2 UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo v2 UIKit
+//
+//  Created by Alex Hwan on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
